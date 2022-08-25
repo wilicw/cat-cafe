afd28a5934778346b22c6f0d0a3bfc5665d1b576
William Chang <wilicw1346@gmail.com>
2022-08-25 22:26:12 +0800

    Add osm

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { MapContainer, TileLayer, useMap } from 'react-leaflet';
 
 function App() {
   return (
-    <div className="ts-box">
-      <div className="ts-content">
-
-      </div>
+    <div className="ts-box" style={{
+      width: 500,
+      height: 500
+    }} >
+      <MapContainer
+        center={[25.0481279, 121.5139046]}
+        zoom={15}
+      >
+        {/* <TileLayer
+          url="https://wmts.nlsc.gov.tw/wmts/EMAP2/default/EPSG:3857/{z}/{y}/{x}"
+        /> */}
+        <TileLayer
+          url="https://stamen-tiles.a.ssl.fastly.net/terrain/{z}/{x}/{y}@2x.png"
+        />
+      </MapContainer>
     </div>
   );
 }
