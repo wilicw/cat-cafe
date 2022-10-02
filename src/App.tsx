74975a7dcd0a387a7605afdcfdb90cdb57b1af16
William Chang <wilicw1346@gmail.com>
2022-10-02 22:25:14 +0800

    Use double layer map to prevent 404

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,9 @@ function App() {
           url="https://wmts.nlsc.gov.tw/wmts/EMAP2/default/EPSG:3857/{z}/{y}/{x}"
         /> */}
         <TileLayer
+          url="https://stamen-tiles.a.ssl.fastly.net/terrain/{z}/{x}/{y}.png"
+        />
+        <TileLayer
           url="https://stamen-tiles.a.ssl.fastly.net/terrain/{z}/{x}/{y}@2x.png"
         />
       </MapContainer>
